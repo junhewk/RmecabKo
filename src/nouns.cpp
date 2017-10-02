1a80063b6879d00fc5e35beca39265864b6b91f9
junhewk <junhewk.kim@gmail.com>
2017-10-02 13:04:24 -0400

    Error Correction: Cpp function

--- src/nouns.cpp
+++ src/nouns.cpp
@@ -3,9 +3,15 @@
 #include <iostream>
 #include <string>
 
-#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+#if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
 
 #include <mecab.h>
+
+#elif defined(_WIN32)
+
+#define OS_Windows
+
+#endif
 
 #define CHECK(eval) if (! eval) { \
 const char *e = tagger ? tagger->what() : MeCab::getTaggerError();\
@@ -17,22 +23,28 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 List nounsRcpp(const CharacterVector & phrase, const CharacterVector & dic){
-  
+
+#ifdef OS_Windows
+
+return R_NilValue;
+
+#else
+
   const char * dicpath = as<const char *>(dic);
-  
+
   MeCab::Tagger *tagger = MeCab::createTagger(dicpath);
   CHECK(tagger);
-  
+
   // Gets Node object.
   Rcpp::List tagged_list;
-  
+
   for (int i = 0; i < phrase.size(); i++) {
     std::string text = Rcpp::as<std::string>(phrase(i));
     const MeCab::Node* node = tagger->parseToNode(text.c_str());
     CHECK(node);
 
     Rcpp::CharacterVector tagged;
-    
+
     for (; node; node = node->next) {
       if (node->stat == MECAB_BOS_NODE)
         ;
@@ -49,18 +61,7 @@ List nounsRcpp(const CharacterVector & phrase, const CharacterVector & dic){
 
   delete tagger;
   return tagged_list;
-}
-
-#elif defined(_WIN32)
-
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-List nounsRcpp(const CharacterVector & phrase, const CharacterVector & dic) {
-  // mecab in Windows needs to be compiled by VC++, but Rcpp doesn't compatible with it.
-  Rcpp::List returnValue;
-  returnValue.push_back("Rcpp doesn't support VC++.");
-  return returnValue;
-}
-
+  
 #endif
+}
+
